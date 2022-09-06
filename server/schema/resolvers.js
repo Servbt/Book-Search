7d945f3272d5d7601e838a1bbc2d78eeb1d56e49
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-09-05 22:34:26 -0400

    with some help I've gotten the mutations down

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -12,8 +12,47 @@ const resolvers = {
             }
             throw new AuthenticationError('Not logged in');
         }
-    }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne( { email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const correctPass = await user.isCorrectPassword(password);
+            if(!correctPass) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: {savedBooks: book} },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user };
+        }
+    }
   };
 
   module.exports = resolvers;
