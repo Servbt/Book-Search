b59d1aa61da36b8c631ad116fe6a406c0c2981ea
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-09-05 22:26:02 -0400

    added some more packages and changed a little in server js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+//db connection
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
